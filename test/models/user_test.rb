5f160b1daa75b062d736a7cf123b274d1dbc52e7
ManyBaby <hello@hacker-vaillant.org>
2021-03-31 13:23:00 +0200

    fix: :fire: Remove test folder

--- test/models/user_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class UserTest < ActiveSupport::TestCase
-  context 'validations' do
-    should validate_presence_of(:username)
-    should validate_uniqueness_of(:email).with("sta@douar.tech", "STa@douar.tech")
-  end
-end
