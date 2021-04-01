4d5537b686aa62b0e4d20c606ede3e4a8a13c146
ManyBaby <hello@hacker-vaillant.org>
2021-04-01 10:28:40 +0200

    fix: :bug: Fix syntax error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[firstname lastname]
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[firstname lastname])
   end
 end
