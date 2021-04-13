100201bf336aa821ff894c7d89a70b021a76bdea
ManyBaby <hello@hacker-vaillant.org>
2021-04-13 14:27:51 +0200

    feat: :sparkles: Active menu item helper

--- app/components/navbar_component.rb
+++ app/components/navbar_component.rb
@@ -2,7 +2,11 @@
 
 class NavbarComponent < ViewComponent::Base
   def initialize(menu_items:, user:)
-    @menu_items = menu_items
+    @menu_items = main_menu_items
     @user = user
+  end
+
+  def active?(path)
+    helpers.menu_item_active?(path)
   end
 end
---
+++ app/helpers/menu_helper.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+# Here are the helper methods that are useful for the whole application
+
+require 'ostruct'
+
+module MenuHelper
+  MAIN_MENU_ITEMS =
+    [
+      {
+        label: "About",
+        path: "/about"
+      },{
+        label: "Contact",
+        path: "/contact"
+      },{
+        label: "Home",
+        path: "/"
+      }
+    ]
+
+  def main_menu_items
+    MAIN_MENU_ITEMS.map do |menu_item|
+      OpenStruct.new(menu_item)
+    end
+  end
+
+  def menu_item_active?(path)
+    request.path == path
+  end
+end
