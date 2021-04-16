dc740be01c0a177f3de273503e942a1efbc89af8
ManyBaby <hello@hacker-vaillant.org>
2021-04-16 13:22:55 +0200

    refactor: :recycle: Refactor tests

--- spec/feature/onboarding_spec.rb
+++ spec/feature/onboarding_spec.rb
@@ -5,23 +5,25 @@ require "rails_helper"
 
 RSpec.describe "Onboarding", type: :feature do
   context "As a First Time Visitor" do
+    let(:subdomain) { 'douar' }
     describe "I can create my own Learning Community" do
-      within_subdomain("douar") do
-        it "So that I become the administrator" do
-          visit "/"
-          click_on "Create my learning community"
-          fill_in "community[name]", with: "Douar Tech"
-          fill_in "community[subdomain]", with: "douar"
-          fill_in "community[email]", with: "hanae@douar.tech"
-          fill_in "community[username]", with: "hanaeB"
-          fill_in "community[password]", with: "p@$$w0rd"
-          click_on "Create"
-          expect(page).to have_content(
-            "Well done! You are now the administrator of the"\
-            " Douar Tech Learning Community"
-          )
-          expect(page).to have_title "Onboarding"
-        end
+      it "So that I become the administrator" do
+        visit "/"
+        click_on "Create my learning community"
+        fill_in "community[name]", with: "Douar Tech"
+        fill_in "community[subdomain]", with: subdomain
+        fill_in "community[email]", with: "hanae@douar.tech"
+        fill_in "community[username]", with: "hanaeB"
+        fill_in "community[password]", with: "p@$$w0rd"
+        click_on "Create"
+
+        Capybara.default_host = "http://#{subdomain}.lvh.me"
+        expect(page).to have_content(
+          "Well done! You are now the administrator of the"\
+          " Douar Tech Learning Community"
+        )
+        expect(page).to have_title "Onboarding"
+        Capybara.default_host = "http://www.lvh.me"
       end
     end
   end
