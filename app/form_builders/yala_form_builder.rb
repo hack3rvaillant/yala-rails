fef7dcb474de997ff7539d5097a12415203edad7
ManyBaby <hello@hacker-vaillant.org>
2021-04-18 00:31:33 +0200

    style: :rotating_light: StandardRB

--- app/form_builders/yala_form_builder.rb
+++ app/form_builders/yala_form_builder.rb
@@ -6,7 +6,7 @@ class YalaFormBuilder < ActionView::Helpers::FormBuilder
   def labelled_text_field(attribute, args = {})
     @template.content_tag(:div, class: "form-group") do
       label(attribute, args[:label], class: "label") +
-      text_field(attribute, merge_args(attribute, args)) +
+        text_field(attribute, merge_args(attribute, args)) +
         display_errors(attribute)
     end
   end
@@ -14,7 +14,7 @@ class YalaFormBuilder < ActionView::Helpers::FormBuilder
   def labelled_email_field(attribute, args = {})
     @template.content_tag(:div, class: "form-group") do
       label(attribute, args[:label], class: "label") +
-        email_field(attribute,merge_args(attribute, args)) +
+        email_field(attribute, merge_args(attribute, args)) +
         display_errors(attribute)
     end
   end
@@ -22,7 +22,7 @@ class YalaFormBuilder < ActionView::Helpers::FormBuilder
   def labelled_password_field(attribute, args = {})
     @template.content_tag(:div, class: "form-group") do
       label(attribute, args[:label], class: "label") +
-        password_field(attribute,merge_args(attribute, args)) +
+        password_field(attribute, merge_args(attribute, args)) +
         display_errors(attribute)
     end
   end
@@ -54,6 +54,6 @@ class YalaFormBuilder < ActionView::Helpers::FormBuilder
   end
 
   def merge_args(attribute, args)
-    args.merge!({ class: merge_error_class(attribute, args) })
+    args.merge!({class: merge_error_class(attribute, args)})
   end
 end
