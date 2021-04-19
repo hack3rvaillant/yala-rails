3a7eed8e15fb701011e049ff6b896aeb04fb039c
ManyBaby <hello@hacker-vaillant.org>
2021-04-19 15:43:49 +0200

    feat: :sparkles: Generate devise controllers and views

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,10 +1,14 @@
 class Community < ApplicationRecord
   resourcify
   validates :name, presence: true
-  validates :subdomain, presence: true
-  validates :subdomain, uniqueness: true
-  validates :subdomain, exclusion: {in: Subdomain::BLACKLIST}
-  # TODO, validates :domain, format: {}
+  validates :slug, presence: true
+  validates :slug, uniqueness: true
+  validates :slug, exclusion: {in: Slug::BLACKLIST}
 
   has_many :users, dependent: :destroy
+
+  def to_param
+    return nil unless persisted?
+    slug
+  end
 end
